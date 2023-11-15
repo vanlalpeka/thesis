db9ead4a80fb385b49728cf377d55ab02f852222
Vanlal Peka <vanlalpeka88@gmail.com>
2023-11-15 19:00:32 +0100

    added logging

--- jobs.py
+++ jobs.py
@@ -18,6 +18,21 @@ import json
 import os
 import datetime
 
+#importing the module 
+import logging 
+
+#now we will Create and configure logger 
+logging.basicConfig(filename=f"std_{datetime.datetime.today()}.log", 
+					format='%(asctime)s %(message)s', 
+					filemode='w') 
+
+#Let us Create an object 
+logger=logging.getLogger() 
+
+#Now we are going to Set the threshold of logger to DEBUG 
+logger.setLevel(logging.DEBUG) 
+
+
 # Replace dataset_id with the   ID of the dataset you want to load
 dataset = openml.datasets.get_dataset(
     dataset_id= 42175,  # CreditCardFraudDetection
@@ -34,14 +49,16 @@ X, y, _, _ = dataset.get_data(target=dataset.default_target_attribute)
 # random_state = 42  # You can set a random seed for reproducibility
 X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.2, random_state=42)
 
+logger.info('START')
 
 with open("configs.json","r") as f:
     configs=json.loads(f.read())
 
-runtime, extract, auc, submodels, altnorm = [], [], [], [], []
+runtime, extract, auc, no_submodels, submodel, altnorm = [], [], [], [], [], []
 for config in configs:
+    logger.info(f'Calling sean(altnorm={config["altnorm"]}, no_submodels={config["no_submodels"]}, submodel={config["submodel"]}, prep=[], extract={config["extract"]})')
     start_time = time.time()
-    pred = sean(X_train, X_test, altnorm=True, no_submodels = config["submodel"], prep=1, extract=config["extract"])
+    pred = sean(X_train, X_test, altnorm=config["altnorm"], no_submodels = config["no_submodels"], prep=[], extract=config["extract"])
     end_time = time.time()
     rt = end_time - start_time
     runtime.append(rt)
@@ -50,8 +67,7 @@ for config in configs:
     altnorm.append(config["altnorm"])
     t_auc = roc_auc_score(y_test, pred)
     auc.append(t_auc)
-    print(f'Feature extraction type {config["extract"]} with {config["submodel"]} submodels completed at \
-             {datetime.datetime.fromtimestamp(start_time / 1000).strftime("%H:%M:%S")}. AUC: {t_auc}. Runtime: {rt}')
+    logger.info(f'Feature extraction type: {config["extract"]} Submodel: {config["submodel"]} Count: {config["no_submodels"]} Altnorm: {config["altnorm"]} AUC: {t_auc} Runtime: {rt}')
 # print('\nROC_AUC: {} '.format(roc_auc_score(y_test, pred)))
 
 # with open('output_data.csv', 'a', newline='') as csvfile:
@@ -62,16 +78,17 @@ for config in configs:
 #     if csvfile.tell() == 0:
 #         writer.writeheader()
 
-df = pd.DataFrame(
-        {
-            'Date': datetime.date.today(),
-            'Dataset': 'CC Fraud', 
-            'Altnorm': altnorm, 
-            'Feature Extraction': extract,  
-            'Submodel Count':submodels,
-            'Runtime': runtime, 
-            'ROC-AUC': auc
-        }
-    )
+# df = pd.DataFrame(
+#         {
+#             'Date': datetime.date.today(),
+#             'Dataset': 'CC Fraud', 
+#             'Altnorm': altnorm, 
+#             'Feature Extraction': extract,  
+#             'Submodel Count':submodels,
+#             'Runtime': runtime, 
+#             'ROC-AUC': auc
+#         }
+#     )
 
-df.to_csv('output_data.csv')
+# df.to_csv('output_data.csv')
+logger.info('END')
--- sean.py
+++ sean.py
@@ -1,14 +1,9 @@
-# import importlib.util
-
-# if importlib.util.find_spec('pydbm') is None:
-#     !pip install pydbm
-# else:
-#     print('pydbm is already installed')
+# !pip install openml
 
 import numpy as np
 import pandas as pd
 
-from sklearn.linear_model import LinearRegression, LassoCV
+from sklearn.linear_model import LinearRegression, LassoCV, RidgeClassifierCV
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.decomposition import FastICA, PCA, NMF
@@ -28,9 +23,7 @@ import itertools
 # from pydbm.activation.logistic_function import LogisticFunction
 # from pydbm.activation.tanh_function import TanhFunction
 # from pydbm.optimization.optparams.sgd import SGD
-# from pydbm.optimization.optparams.adam import Adam
 # from pydbm.dbm.restrictedboltzmannmachines.rt_rbm import RTRBM
-
 
 def generate_subsets(sequence, leng=3):
     print('generate_subsets')
@@ -68,76 +61,19 @@ class Autoencoder(Model):
         decoded = self.decoder(encoded)
         return decoded
 
-# def RBM(xx,txx,num_feats_rel)
-#     learning_rate = 0.01
-#     training_epochs = 50
-#     batch_size = 100
-#     n_hidden = int(math.ceil(num_feats_rel*xx.shape[1]))
-#     n_visible = xx.shape[1]
 
-#     X = tf.placeholder("float", [None, n_visible], name='X')
-#     X_noise = tf.placeholder("float", [None, n_visible], name='X_noise')
-
-#     # RBM parameters
-#     W = tf.Variable(tf.random_normal([n_visible, n_hidden], 0.01), name='W')
-#     b_visible = tf.Variable(tf.zeros([n_visible]), name='b_visible')
-#     b_hidden = tf.Variable(tf.zeros([n_hidden]), name='b_hidden')
-
-#     # activation functions
-#     def sigmoid(x):
-#         return 1. / (1. + tf.exp(-x))
-
-#     def sample_prob(probs):
-#         return tf.nn.relu(tf.sign(probs - tf.random_uniform(tf.shape(probs))))
-
-#     # the positive phase, which calculates the expected value of the hidden layer activations given the input data
-#     h_prob = sigmoid(tf.matmul(X_noise, W) + b_hidden)
-#     h_state = sample_prob(h_prob)
-
-#     # the negative phase, which calculates the expected value of the visible layer activations given the hidden layer activations
-#     v_prob = sigmoid(tf.matmul(h_state, tf.transpose(W)) + b_visible)
-#     v_state = sample_prob(v_prob)
-
-#     # the reconstruction error, which measures the difference between the input data and the reconstructed data
-#     err = tf.reduce_mean(tf.square(X - v_state))
-
-#     # the training operation, which updates the RBM parameters to minimize the reconstruction error.
-#     train_op = tf.train.GradientDescentOptimizer(learning_rate).minimize(err)
-
-#     # Train the RBM
-#     init = tf.global_variables_initializer()
-#     with tf.Session() as sess:
-#       sess.run(init)
-#       for epoch in range(training_epochs):
-#         total_batch = int(xx.shape[0] / batch_size)
-#         for i in range(total_batch):
-#           batch_xs, _ = mnist.train.next_batch(batch_size)
-#           batch_xs_noise = batch_xs + 0.3*np.random.randn(*batch_xs.shape)
-#           batch_xs_noise = np.clip(batch_xs_noise, 0., 1.)
-#           batch_xs = np.clip(batch_xs, 0., 1.)
-#           _, c = sess
-#       sess.run(train_op, feed_dict={X: batch_xs, X_noise: batch_xs_noise})
-#       print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(c))
-#     print("RBM training finished!")
-
-#     # Test the RBM on the test data
-#     batch_xs, _ = mnist.test.next_batch(10)
-#     batch_xs_noise = batch_xs + 0.3*np.random.randn(*batch_xs.shape)
-#     batch_xs_noise = np.clip(batch_xs_noise, 0., 1.)
-#     batch_xs = np.clip(batch_xs, 0., 1.)
-#     err_test = sess.run(err, feed_dict={X: batch_xs, X_noise: batch_xs_noise})
-#     print("RBM test error:", err_test)
-
-
-def ZCA(df):
+# Calculates ZCA components from xx. Applies that to both xx and txx
+def ZCA(xx, txx):
     # Calculate the mean of each of the columns
-    mean = np.mean(df, axis=0)
+    mean_xx = np.mean(xx, axis=0)
+    mean_txx = np.mean(txx, axis=0)
 
     # Center the data by subtracting the mean
-    centered_data = df - mean
+    centered_xx = xx - mean_xx
+    centered_txx = txx - mean_txx
 
     # Calculate the covariance matrix
-    covariance_matrix = np.cov(centered_data, rowvar=False)
+    covariance_matrix = np.cov(centered_xx, rowvar=False)
 
     # Perform eigenvalue decomposition on the covariance matrix
     eigenvalues, eigenvectors = np.linalg.eigh(covariance_matrix)
@@ -146,23 +82,13 @@ def ZCA(df):
     zca_components = np.dot(eigenvectors, np.dot(np.diag(1.0 / np.sqrt(eigenvalues + 1e-5)), eigenvectors.T))
 
     # Apply ZCA whitening to the data
-    zca_whitened_data = np.dot(centered_data, zca_components)
+    zca_whitened_xx = np.dot(centered_xx, zca_components)
+    zca_whitened_txx = np.dot(centered_txx, zca_components)
 
-    return zca_whitened_data
+    return zca_whitened_xx, zca_whitened_txx
 
 
-def sean(x,tx,cept=False,justlin=True,projections=False,no_submodels=5000,num_feats_rel=0.2,num_feats_abs=10,order=2,relative_feats=True,altnorm=True,min_feats=3, prep=0, extract=0):
-    #use intercept? No ensemble learning with an intercept
-    #use linear regression?
-    #feature selector
-    #number of submodels
-    #number of features
-    #taylor order of features
-    #relative num_feats?
-    #normalize between 0.5 and 1 instead 0 and 1
-    #minimum number of feats
-    # preprocessing
-    # Feature extractor
+def sean(x, tx, cept=False, no_submodels=5000, num_feats_rel=0.2, num_feats_abs=10, order=2, relative_feats=True, altnorm=True, min_feats=3, prep=[], extract='ica', submodel='lin'):
 
     # print('In SEAN')
     # print('Params: number of submodels {} \n Linear? {} \n Feature Selector? {} \n Relative features? {} \n Altnorm? {} \n Min Features? {} \n'.format(no_submodels, justlin,projections,relative_feats,altnorm,min_feats))
@@ -170,17 +96,21 @@ def sean(x,tx,cept=False,justlin=True,projections=False,no_submodels=5000,num_fe
     # PRE-PROCESSING
     ######################################################################################
     def pre_process(xx,txx):
-        if prep == 1:        # [0,1] Normalization
-            scaler = MinMaxScaler()
-            x = scaler.fit_transform(xx)
-            # x = pd.DataFrame(x, columns=xx.columns)
-            # tx = MinMaxScaler().fit_transform(txx)
-            tx = scaler.transform(txx)
-            # tx = pd.DataFrame(tx, columns=txx.columns)
+        # [0,1] Normalization
+        scaler = MinMaxScaler()
+        x = scaler.fit_transform(xx)
+        tx = scaler.transform(txx)
+        if altnorm: # [0.5,1] Normalization
+          x=(1+x)/2
+          tx=(1+tx)/2
 
-            if altnorm: # [0.5,1] Normalization
-              x=(1+x)/2
-              tx=(1+tx)/2
+        # tabular dataset
+        if len(x.shape) == 2:
+            pass
+
+        # image dataset
+        if len(x.shape) == 3:
+            pass
 
         return x,tx
 
@@ -191,55 +121,54 @@ def sean(x,tx,cept=False,justlin=True,projections=False,no_submodels=5000,num_fe
         n_components = int(math.ceil(num_feats_rel*xx.shape[1]))
 
         match extract:
-            case 1:   # ZCA +RBM
-                # ZCA
-                x = ZCA(xx)
-                tx = ZCA(txx)
+            case "rbm":   # ZCA +RBM
+                # ZCA(): Calculates ZCA components from xx. Applies that to both xx and txx
+                x, txx = ZCA(xx, txx)
 
-                # # The `Client` in Builder Pattern for building RTRBM.
-                # rt_rbm = RNNRBM(
-                #     # The number of units in visible layer.
-                #     visible_num=observed_arr.shape[2],
-                #     # The number of units in hidden layer.
-                #     hidden_num=100,
-                #     # The activation function in visible layer.
-                #     visible_activating_function=LogisticFunction(),
-                #     # The activation function in hidden layer.
-                #     hidden_activating_function=LogisticFunction(),
-                #     # The activation function in RNN layer.
-                #     rnn_activating_function=LogisticFunction(),
-                #     # is-a `OptParams`.
-                #     opt_params=SGD(),
-                #     # Learning rate.
-                #     learning_rate=1e-05
-                # )
+                # The `Client` in Builder Pattern for building RTRBM.
+                rt_rbm = RNNRBM(
+                    # The number of units in visible layer.
+                    visible_num=observed_arr.shape[2],
+                    # The number of units in hidden layer.
+                    hidden_num=100,
+                    # The activation function in visible layer.
+                    visible_activating_function=LogisticFunction(),
+                    # The activation function in hidden layer.
+                    hidden_activating_function=LogisticFunction(),
+                    # The activation function in RNN layer.
+                    rnn_activating_function=LogisticFunction(),
+                    # is-a `OptParams`.
+                    opt_params=SGD(),
+                    # Learning rate.
+                    learning_rate=1e-05
+                )
 
-                # # Learning.
-                # rt_rbm.learn(
-                #     # The `np.ndarray` of observed data points.
-                #     observed_arr,
-                #     # Training count.
-                #     training_count=1000,
-                #     # Batch size.
-                #     batch_size=20
-                # )
+                # Learning.
+                rt_rbm.learn(
+                    # The `np.ndarray` of observed data points.
+                    observed_arr,
+                    # Training count.
+                    training_count=1000,
+                    # Batch size.
+                    batch_size=20
+                )
 
-                # # error
-                # error_arr = rt_rbm.rbm.get_reconstruct_error_arr()
+                # error
+                error_arr = rt_rbm.rbm.get_reconstruct_error_arr()
 
-                # plt.figure(figsize=(20, 10))
-                # plt.plot(error_arr)
-                # plt.ylabel("MSE")
-                # plt.xlabel("Epochs")
-                # plt.show()
+                plt.figure(figsize=(20, 10))
+                plt.plot(error_arr)
+                plt.ylabel("MSE")
+                plt.xlabel("Epochs")
+                plt.show()
 
-            case 2:   # tSNE
+            case "tsne":   # tSNE
 
                 # Dimensionality reduction to improve performance
                 if xx.shape[1] > 10:
-                    xx = PCA(n_components=10).fit_transform(xx)
-                if txx.shape[1] > 10:
-                    txx = PCA(n_components=10).fit_transform(txx)
+                    pca = PCA(n_components=10)
+                    xx = pca.fit_transform(xx)
+                    txx = pca.transform(txx)
 
                 # n_components = number of output dimensions (usually 2 for 2D visualization)
                 # perplexity = a hyperparameter that controls the balance between preserving global and local structure
@@ -248,24 +177,27 @@ def sean(x,tx,cept=False,justlin=True,projections=False,no_submodels=5000,num_fe
                 print('\n tSNE extract_features 2')
                 x = tsne.fit_transform(xx)
                 print('\n tSNE extract_features 3')
-                tx = tsne.fit_transform(txx)
+                tx = tsne.transform(txx)
                 print('\n tSNE extract_features 4')
 
-            case 3:   # PCA
-                x = PCA(n_components=10).fit_transform(xx)
-                tx = PCA(n_components=10).fit_transform(txx)
+            case "pca":   # PCA
+                pca = PCA(n_components=10)
+                x = pca.fit_transform(xx)
+                tx = pca.transform(txx)
 
-            case 4:   # ICA
-                x = FastICA(n_components=10, whiten='unit-variance').fit_transform(xx)
-                tx = FastICA(n_components=10, whiten='unit-variance').fit_transform(txx)
+            case "ica":   # ICA
+                fastICA = FastICA(n_components=10, whiten='unit-variance')
+                x = fastICA.fit_transform(xx)
+                tx = fastICA.transform(txx)
 
-            case 5:   # NMF
+            case "nmf":   # NMF
                 # NMF is a non-convex optimization problem, so the results may vary with different initializations
-                x = NMF(n_components=10, init='random', random_state=0).fit_transform(xx)
-                tx = NMF(n_components=10, init='random', random_state=0).fit_transform(txx)
-                # xx_features = NMF(n_components=10, init='random', random_state=0).components_
+                nmf = NMF(n_components=10, init='random', random_state=0)
+                x = nmf.fit_transform(xx)
+                tx = nmf.transform(txx)
+                # xx_features = nmf.components_
 
-            case 6:   # Autoencoder
+            case "ae":   # Autoencoder
                 shape = xx.shape[1:]
                 latent_dim = int(math.ceil(num_feats_rel*shape[0]))
                 autoencoder = Autoencoder(latent_dim, shape)
@@ -286,9 +218,17 @@ def sean(x,tx,cept=False,justlin=True,projections=False,no_submodels=5000,num_fe
 
     ######################################################################################
     # FEATURE ENGINEERING
+    # First, standardize (mean=0, variance=1) the data ; second, generate interaction terms.
+    # This is to prevent multicollinearity among the new (engineered) features
+    # https://www.tandfonline.com/doi/abs/10.1080/01621459.1980.10477430
     ######################################################################################
     def engineer_features(x,tx,order=order):
         # print('select_features')
+
+        scaler = StandardScaler()
+        x = scaler.fit_transform(x)
+        tx = scaler.transform(tx)
+
         #all ordered subsequences up to count of length up to order
         orderings = list(itertools.chain.from_iterable(itertools.combinations(range(x.shape[1]),i) for i in range(1,order+1)))
 
@@ -315,23 +255,10 @@ def sean(x,tx,cept=False,justlin=True,projections=False,no_submodels=5000,num_fe
         x = np.array(xx).T
         tx = np.array(txx).T
 
-        # select features
-        if projections:
-            matrix=np.random.uniform(0,1,(x.shape[1],num_feats))
-            x=np.matmul(x,matrix)
-            tx=np.matmul(tx,matrix)
-            scaler=MinMaxScaler()
-            x=scaler.fit_transform(x)
-            tx=scaler.transform(tx)
-            return x,tx
-        else:
-            count=num_feats
-            # print('num_feats {}; x shape {}; tx shape {}'.format(num_feats, x.shape, tx.shape))
-
-            if count>x.shape[1]:
-                count=x.shape[1]
-            feats=np.random.choice(range(x.shape[1]),count,replace=False)
-            return x[:,feats],tx[:,feats]
+        if num_feats > x.shape[1]:
+            num_feats = x.shape[1]
+        feats = np.random.choice(range(x.shape[1]), num_feats, replace=False)
+        return x[:,feats],tx[:,feats]
 
     # no_of_covariates=int(np.ceil(x.shape[1]/10))#3
 
@@ -348,10 +275,20 @@ def sean(x,tx,cept=False,justlin=True,projections=False,no_submodels=5000,num_fe
 
         # eqn. 2 from the DEAN paper
         goal=np.ones(len(x))
-        if justlin:
-            cv=LinearRegression(fit_intercept=cept).fit(x,goal)
-        else:
-            cv=LassoCV(fit_intercept=False).fit(x,goal)
+
+        match submodel:
+            case "lin":
+                cv = LinearRegression(fit_intercept=cept).fit(x,goal)
+            case "lasso":
+                cv = LassoCV(fit_intercept=False).fit(x,goal)
+            case "ridge":
+                cv = RidgeClassifierCV(fit_intercept=False).fit(x,goal)
+            case "elastic":
+                cv = ElasticNetCV(fit_intercept=False).fit(x,goal)
+            case "log":
+                cv = LogisticRegressionCV(fit_intercept=False).fit(x,goal)
+            case _:
+                raise Exception("Please specify a submodel type")            
 
         # eqn. 4 from the DEAN paper
         meanv=np.mean(cv.predict(x))
@@ -360,8 +297,7 @@ def sean(x,tx,cept=False,justlin=True,projections=False,no_submodels=5000,num_fe
         return pred
 
     scores=[]
-    # for i in tqdm(range(no_submodels)):
-    for i in range(no_submodels):
+    for i in tqdm(range(no_submodels)):
         # print('sub model {} of {}'.format(i,no_submodels))
         # print('Selected features of X : {}'.format(np.array(xx).shape))
         pred=one_model(x,tx)
@@ -375,6 +311,7 @@ def sean(x,tx,cept=False,justlin=True,projections=False,no_submodels=5000,num_fe
 
     # eqn. 5 from the DEAN paper
     return np.mean(scores,axis=0)
+
 
 # executes only when ran directly, not when this file is imported into another python file
 if __name__ == '__main__':
