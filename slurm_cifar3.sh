3b7f8270f058072e91eed1b2a28921aea6935df2
peka <vanlalpeka88@gmail.com>
2024-04-20 22:06:03 +0530

    slurm jobs for images

--- slurm_cifar3.sh
+++ slurm_cifar3.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --time=329:55:00
-#SBATCH --array=1-4537%50     # Number of parameter sets
+#SBATCH --array=2001-3000%50     # Number of parameter sets
 #SBATCH --mem=40000
 #SBATCH --partition=ultralong
-#SBATCH --job-name=mnist
+#SBATCH --job-name=cifar10
 #SBATCH --qos=normal
-#SBATCH -o ./log/mnist.%A.%a.out # STDOUT
-#SBATCH -e ./log/mnist.%A.%a.err # STD error
+#SBATCH -o ./log/cifar10.%A.%a.out # STDOUT
+#SBATCH -e ./log/cifar10.%A.%a.err # STD error
 
 # Load necessary modules or set up environment if needed
 # module purge
@@ -14,5 +14,5 @@ module load python/3.11.7-gcc114-base
 
 # Run the Python script with the corresponding parameter from the CSV file
 export PYTHONUNBUFFERED=TRUE
-python3 job_mnist.py $SLURM_ARRAY_TASK_ID
+python3 job_cifar10.py $SLURM_ARRAY_TASK_ID
 
---
+++ slurm_mnist1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --time=329:55:00
+#SBATCH --array=1-1000%50     # Number of parameter sets
+#SBATCH --mem=40000
+#SBATCH --partition=ultralong
+#SBATCH --job-name=mnist
+#SBATCH --qos=normal
+#SBATCH -o ./log/mnist.%A.%a.out # STDOUT
+#SBATCH -e ./log/mnist.%A.%a.err # STD error
+
+# Load necessary modules or set up environment if needed
+# module purge
+module load python/3.11.7-gcc114-base
+
+# Run the Python script with the corresponding parameter from the CSV file
+export PYTHONUNBUFFERED=TRUE
+python3 job_mnist.py $SLURM_ARRAY_TASK_ID
+
